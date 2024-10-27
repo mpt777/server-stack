a81fdf4a5325f76292bed1e00a42becd0f235b71
Marshal <mptaylor777@gmail.com>
2024-10-27 09:05:08 -0600

    push

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,11 +1,10 @@
 <script>
      // Finally, your application's global stylesheet (sometimes labeled 'app.css')
     import "../app.css"
-    import "$static/fonts/remix/remixicon.css"
+    
 
 	import { initializeStores } from '@skeletonlabs/skeleton';
 
 	initializeStores();
 </script>
-
 <slot/>
