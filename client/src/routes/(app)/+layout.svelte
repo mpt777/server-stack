628ba2163b10aa6f92f713aa2cf4be9a744e5ef2
Marshal <mptaylor777@gmail.com>
2024-11-04 17:41:40 -0700

    layout

--- client/src/routes/(app)/+layout.svelte
+++ client/src/routes/(app)/+layout.svelte
@@ -40,21 +40,23 @@
 
 <div class="grid h-screen grid-rows-[auto_1fr_auto]">
 	<!-- Header -->
-	<header class="bg-surface-100-800-token p-4 shadow-md z-10">
+	<header class="bg-surface-50-900-token p-4 shadow-md z-10">
 		<div class="flex justify-between">
 		  <div class="h4"><a href="/">Calyps.io</a></div>
-		  <div class="flex gap-1">
+		  <div class="flex gap-4 items-center">
+				<LightSwitch/>
+
 				<a class="cursor-pointer" on:click={drawerOpen}>
 					<i class="text-xl ri-menu-line" ></i>
 				</a>
-				<LightSwitch/>
+				
 			</div>
 		</div>
 	</header>
 	<!-- Grid Columns -->
 	<div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
 	  <!-- Left Sidebar. -->
-	  <aside class="p-1 bg-surface-100-800-token hidden md:block border-r border-surface-500/30">
+	  <aside class="p-1 bg-surface-50-900-token hidden md:block border-r border-surface-500/30">
       <Navigation/>
     </aside>
 	  <!-- Main Content -->
