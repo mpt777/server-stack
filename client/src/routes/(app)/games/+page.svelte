dd8ee7eaf19423ca5929ef62b7c92059687ba7c7
Marshal <mptaylor777@gmail.com>
2025-01-07 19:28:07 -0700

    banshee bash

--- client/src/routes/(app)/games/+page.svelte
+++ client/src/routes/(app)/games/+page.svelte
@@ -1,12 +1,18 @@
 <div class="w-full text-token grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 	
-
-	<div class="card variant-filled card-hover overflow-hidden">
-		<header class="card-header">
-			<h2 class="h2">Banshee Bash</h2>
-		</header>
+	<a href="https://store.steampowered.com/app/3395730/Banshee_Bash/" target="_blank"
+		class="card variant-filled card-hover overflow-hidden">
+		
+		<img src="/images/header.jpg" class="w-full" alt="Banshee Bash Header">
 		<section class="p-4">
-			Coming 2025
+			<div>Banshee Bash is a local-multiplayer party game. </div>
+			<div>Fight with up to 3 friends (or add bots) to battle as a team, or in an epic free-for-all. </div>
+			<div>Collect mana and learn spells to cast your way to victory! </div>
+			<div>Games are quick, and will allow you to level up for unlockable cosmetics to add some flair!</div>
 		</section>
-	</div>
+		<hr>
+		<section class="p-4">
+			<div>Coming Q2 2025</div>
+		</section>
+	</a>
 </div>
