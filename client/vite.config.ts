952e330ed205d01ac94010f251d147a164a91fdd
Marshal <mptaylor777@gmail.com>
2025-02-01 18:18:30 -0700

    new skeleton

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
+		allowedHosts: ['calyps.io'],
 		fs: {
 		  allow: [
 			'/usr/src/app/static', // Add this line to allow access to the static directory
